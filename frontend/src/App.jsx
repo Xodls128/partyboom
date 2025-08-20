698ad46732724db5462bf7627f3f1404077706da
jiyoung <jenn4843@naver.com>
2025-08-21 03:57:44 +0900

    [FE]FEAT: 신고하기 페이지에 맞게 app.jsx 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,21 @@ import Home from "./pages/Home.jsx";
 import Notifications from "./pages/Notifications.jsx";
 import Map from "./pages/Map.jsx";
 import Assist from "./pages/Assist.jsx";
-import Mypage from "./pages/Mypage.jsx";  {/*페이지의 파일명, 경로명 일치시키기 */}
+import Mypage from "./pages/Mypage.jsx";  
 import AppFrame from './components/AppFrame.jsx';
+import ReportUser from './pages/ReportUser.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
       <AppFrame>
         <Routes>
-          <Route index element={<Home />} />
+          <Route index element={<Home />} /> 
           <Route path="/notifications" element={<Notifications />} />
           <Route path="/map" element={<Map />} />
           <Route path="/assist" element={<Assist />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/report/:userId" element={<ReportUser />} /> 
         </Routes>
       </AppFrame>
     </BrowserRouter>
