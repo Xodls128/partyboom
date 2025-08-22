d746cecc397e613be68c7be1b541635a55cb3e74
xodls <pider6@naver.com>
2025-08-23 04:49:12 +0900

    [BE & FE]FEAT: 이미지 가져오는 방식수정(기본이미지를 백 static에서 저장해두고 가져오는 방식으로 수정:이미지없어도 None 반환 안함)

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserDetailView(APIView):
+    def get(self, request):
+        serializer = UserSerializer(request.user, context={"request": request})
+        return Response(serializer.data)
+    
