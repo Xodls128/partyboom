bb7aa44e623ace3e1b3826860c8cb0819eb2122a
xodls <pider6@naver.com>
2025-08-24 01:52:05 +0900

    [BE]FEAT:개발용 static과 배포용 static별도로 설정, 경로 맞게 수정

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -158,10 +158,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'config' / 'static',  # 직접 만든 static 폴더
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles' #배포용 스테틱 설정
 
 AUTH_USER_MODEL = "users.User"
 
