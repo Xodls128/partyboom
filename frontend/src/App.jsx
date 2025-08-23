0c72dbce3e59061349c06cedc4a8c3468e3f2801
xodls <pider6@naver.com>
2025-08-23 21:56:23 +0900

    [FE]FEAT:주석 바꿈

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Partyinfo from './pages/Partyinfo.jsx';
 import Notifications from "./pages/Notifications.jsx";
 import Map from "./pages/Map.jsx";
 // import Assist from "./pages/Assist.jsx";
-// import Mypage from "./pages/Mypage.jsx";  {/*페이지의 파일명, 경로명 일치시키기 */}
+import Mypage from "./pages/Mypage.jsx";  {/*페이지의 파일명, 경로명 일치시키기 */}
 import AppFrame from './components/AppFrame.jsx';
 import KakaoLoginPage from "./pages/KakaoLoginPage.jsx";
 import KakaoCallbackPage from "./pages/KakaoCallbackPage.jsx";
@@ -37,8 +37,8 @@ export default function App() {
               isLoggedIn ? <Notifications /> : <KakaoLoginPage />
             } 
           />
-          {/* <Route path="/assist" element={<Assist />} />
-          <Route path="/mypage" element={<Mypage />} /> */}
+          {/* <Route path="/assist" element={<Assist />} /> */}
+          <Route path="/mypage" element={<Mypage />} />
           <Route path="/map" element={<Map />} />
           <Route path="/partyinfo/:partyId" element={<Partyinfo />} />
           <Route path="/kakao-login" element={<KakaoLoginPage />} />
