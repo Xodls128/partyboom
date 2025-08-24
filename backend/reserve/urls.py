7ceb1fc60e3c84544a913be44345b6bf24696eea
jenajeong <skg4660@gmail.com>
2025-08-25 06:18:18 +0900

    [BE]FIX: 결제창 url 연결

--- backend/reserve/urls.py
+++ backend/reserve/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ReserveJoinView, ReservePayView
+from .views import ReserveJoinView, ReservePayView, ParticipationDetailView
 
 app_name = "reserve"
 
 urlpatterns = [
     path("join/<int:party_id>/", ReserveJoinView.as_view(), name="reserve-join"),
     path("pay/<int:participation_id>/", ReservePayView.as_view(), name="reserve-pay"),
+    path("participation/<int:pk>/", ParticipationDetailView.as_view(), name="participation-detail"),
 ]
