ad2a43ca5e2ff81902e2588928d91d57bc6dc2f3
jenajeong <skg4660@gmail.com>
2025-08-24 11:46:23 +0900

    [FE]FIX: fetch 대신 api 인스턴스 사용 (결제)

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import api from "@/api/axios";
 import Back from '../assets/left_black.svg';
 import './payment.css';
 
