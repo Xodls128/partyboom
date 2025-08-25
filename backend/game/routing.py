10192d7bb6bccad79bf80c6964be2c95dcc10b96
xodls <pider6@naver.com>
2025-08-25 22:07:14 +0900

    [BE]FIX:라우팅 경로추가

--- backend/game/routing.py
+++ backend/game/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/game/round/(?P<round_id>[0-9a-f-]+)/$", consumers.BalanceRoundConsumer.as_asgi()),
+    re_path(r"ws/party/(?P<round_id>\d+)/$", consumers.BalanceRoundConsumer.as_asgi()),
 ]
